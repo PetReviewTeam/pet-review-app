b355419b1092f95630667806c12e67b6d45bc920
Peter.JaeWan.Bae <peter.jae.wan.bae@gmail.com>
2021-04-09 14:38:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
 
 export default App;
+
+// npm install firebase 
+// create firebase.js and export
+// create header component
+// Create pet description component
+    // Img, petdescription and review form in separate divs
+    // submit review button with onclick
+        // on click, empty out review textarea
+    // img, petdescription - in firebase database for MVP
+    // useState
+        // for review
+        // userReview, setUserReview
+        // get value on onChange
+    // useEffect on page load (so empty dependancy)
+// create review component
+// Collapsible chevron bottom left with position absolute
+// When leaving review, select random names from a name array. 
+
+
+
